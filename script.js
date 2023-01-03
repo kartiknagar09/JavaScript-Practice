c5e00bf992b1ce1a57a3b9086b25c30809086b2a
Kartik <nagarkartik82@gmail.com>
2023-01-03 16:04:11 +0530

    JavaScript Practice

---
+++ script.js
@@ -0,0 +1,72 @@
+// // variables:
+
+// let a = 10;
+// let b = 10;
+// console.log(c=a+b);
+
+// // Object OR Nonprimitive datatype:
+
+// const obj = {
+//     "name":"Kartik",
+//     "age": "21",
+//     "Intrest":"Web Devlpementr"
+// }
+// console.log(obj);
+
+// // primitive datatype"
+
+// // nn ss bb u (short form to remind)
+// // Number
+// // null
+// // String
+// // Symbol
+// // boolian
+// // BigInt
+// // undefined
+// let a =12;
+// let b =null;
+// let c = "kartik";
+// let d = Symbol;
+// let e = true;
+// let f = BigInt(5343);
+// let g = undefined;
+
+// console.log(a, b, c, d, e, f, g);
+
+// let a = 15;
+// let b = 5;
+// console.log("a+b =",a+b);
+// console.log("a-b =",a-b);
+// console.log("a*b =",a*b);
+// console.log("a/b =",a/b);
+
+// let a = "kartik";
+// let b = 10;
+// console.log(a+b);
+
+// console.log(typeof a,b);
+
+// const a ={
+//     "name": "kartik",
+//     "age": 21
+// }
+// a["name"] = "sagar"
+
+// console.log(a);
+
+
+// const a = 21;
+// a = 22;
+// console.log(a);
+
+// const dict = {
+//     "variable": "define a value",
+//     "datatype": "see the type of entered data",
+//     "object": "group of values",
+//     "class": "asighn the perticuler values",
+//     "oprator": "To oparate the values"
+// }
+// console.log(dict['object']);
+// console.log(dict.class);
+
+
